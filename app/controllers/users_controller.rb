c1acf3f24c16a77b9360d153fcb2fd90f53803fc
daisuke_rikiyama <walker.cp.48710@gmail.com>
2016-01-28 14:56:05 +0000

    edit create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update]
   def show # 追加
     @user = User.find(params[:id])
   end
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_settings)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # ここを修正
@@ -18,9 +18,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_settings)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def user_settings
+    params.require(:user).permit(:name, :age, :country, :email, :password, :password_confirmation, :profile)
+  end
+  
+  def set_user
+    @user = current_user
   end
 end
