ceeb34c4f589c686fa8973bc537acf2c188f22b2
daisuke_rikiyama <walker.cp.48710@gmail.com>
2016-01-29 13:08:38 +0000

    modified submissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show # 追加
     @user = User.find(params[:id])
   end
@@ -19,7 +21,6 @@ class UsersController < ApplicationController
   end
   
   def edit
-    logged_in_user
   end
   
   def update
@@ -40,7 +41,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :age, :country, :email, :password, :password_confirmation, :profile)
   end
   
-  def set_user
-    @user = current_user
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      flash[:danger] = "Operation of a different user is prohibited"
+      redirect_to root_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
-    validates :name, presence: true, length: { maximum: 50 }
+    validates :name, presence: true, length: { maximum: 20 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :age , numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 120}
-    validates :country, length: { maximum: 50 }
-    validates :profile, length: { maximum: 300 }
+    validates :age , numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 120}, allow_blank: true, on: :update
+    validates :country, length: { maximum: 20 }
+    validates :profile, length: { maximum: 255 }
 end
