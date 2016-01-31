d3700053d207aacf8aeff5f907cfac6e8aa001e8
daisuke_rikiyama <walker.cp.48710@gmail.com>
2016-01-31 05:23:47 +0000

    followings/followers_list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,16 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.following_users.order(:id)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower_users.order(:id)
+  end
+  
   private
   
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
   get    'signup/'   , to: 'users#new'
-  get    'settings/:id' , to: 'users#edit' , as: 'settings'
-  get    'login'    , to: 'sessions#new'
-  post   'login'    , to: 'sessions#create'
-  delete 'logout'   , to: 'sessions#destroy'
+  get    'login'     , to: 'sessions#new'
+  post   'login'     , to: 'sessions#create'
+  delete 'logout'    , to: 'sessions#destroy'
+  get    'users/:id/followings', to: 'users#followings', as: 'followings'
+  get    'users/:id/followers' , to: 'users#followers', as: 'followers'
   
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
